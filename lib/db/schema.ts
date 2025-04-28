0392a8a4cf3f930bf47d92f8f2640a9ebbc1aac6
Saurabh Yadav <74822612+HakashiKatake@users.noreply.github.com>
2025-04-28 21:11:41 +0530

    schemas done, db connection setup

--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -1,6 +1,8 @@
-import {pgTable, text, uuid, integer, boolean} from "drizzle-orm/pg-core";
+import {pgTable, text, uuid, integer, boolean, timestamp} from "drizzle-orm/pg-core";
 import {relations} from "drizzle-orm";
 import path from "path";
+import { create } from "domain";
+
 
 export const files = pgTable("files", {
     id: uuid("id").defaultRandom().primaryKey(),
@@ -9,4 +11,39 @@ export const files = pgTable("files", {
     path: text("path").notNull(),
     size: integer("size").notNull(),
     type: text("type").notNull(),
+
+    //storage info
+
+    fileUrl: text("file_url").notNull(),
+    thumbnailUrl: text("thumbnail_url"),
+
+    //ownership
+
+    userId: text("user_id").notNull(),
+    parentId: uuid("parent_id"), //parent folder if  (null for root folder)
+
+    //file/folder flags
+    isFolder: boolean("is_folder").notNull().default(false),
+    isStarred: boolean("is_starred").notNull().default(false),
+    isTrash: boolean("is_trash").notNull().default(false),
+
+    //timestamps
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
+
+
 })
+
+export const filesRelation = relations(files, ({one, many}) => 
+({
+    parent: one(files, {
+        fields: [files.parentId],
+        references: [files.id],
+    }),
+    childern: many(files)
+}))
+
+//type definations
+
+export const File = typeof files.$inferSelect;
+export const NewFile = typeof files.$inferInsert;
