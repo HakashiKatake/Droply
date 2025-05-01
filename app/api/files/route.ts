a6d1cad59d57444880b6eace6316305f2ef6f3d3
Saurabh Yadav <74822612+HakashiKatake@users.noreply.github.com>
2025-05-01 16:44:14 +0530

    /files route so that we can fetch files and from the sub folders

---
+++ app/api/files/route.ts
@@ -0,0 +1,56 @@
+import { db } from "@/lib/db";
+import { files } from "@/lib/db/schema";
+import { auth } from "@clerk/nextjs/server";
+import { and, eq, isNull } from "drizzle-orm";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+    try {
+        const { userId } = await auth();    
+
+        if (!userId) {
+            return new NextResponse(JSON.stringify({ error: "Unauthorized" }), { status: 401 });
+        }
+
+        const searchParams = request.nextUrl.searchParams
+        const queryUserId = searchParams.get("userId");
+        const parentId = searchParams.get("parentId");
+
+        if (!queryUserId || queryUserId !== userId) {
+            return new NextResponse(JSON.stringify({ error: "Unauthorized" }), { status: 401 });
+        }
+
+        //fetch files from specific folder
+        let userFiles;
+        if (parentId) {
+            await db
+                .select()
+                .from(files)
+                .where(
+                    and(
+                        eq(files.userId, userId),
+                        eq(files.parentId, parentId),
+                        
+                    )
+                )
+        } else {
+            userFiles = await db
+                .select()
+                .from(files)
+                .where(
+                    and(
+                        eq(files.userId, userId),
+                        isNull(files.parentId)
+                    )
+                )
+        }
+
+        return NextResponse.json(userFiles);
+
+
+
+
+    } catch (error) {
+        return new NextResponse(JSON.stringify({ error: "Failed to fetch files" }), { status: 500 });
+    }
+}
